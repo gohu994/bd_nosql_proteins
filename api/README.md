5e46d531cbf1a283d8f410e0e5c23421a133703f
Damien ETHEVE <damien.etheve@telecomnancy.eu>
2021-12-14 15:23:51 +0100

    FEAT: add api that allows you to insert teh similarity in neo4j

---
+++ api/README.md
@@ -0,0 +1,5 @@
+## Commande curl pour insérer les similarité dans la bdd
+
+```bash
+curl -i -X POST -H 'Content-Type: application/json' -d '{ "name": "P2" }' http://localhost:5000/protein
+```
---
+++ api/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request, abort, Response
+from flask_restful import Api, Resource
+# TODO:  add swagger specs
+# from flask_restful_swagger import swagger
+
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+# @api.route("/users/")
+class Protein(Resource):
+  def post(self):
+    """
+      Insert protein similarity in neo4j
+    """
+    data = request.get_json()
+    print(data['name'])
+    
+    return Response(response=json.dumps({"Status": "Data inserted"}),
+                  status=200,
+                  mimetype='application/json')
+
+# resources 
+api.add_resource(Protein, "/protein")
+
+# main
+if __name__ == "__main__":
+  app.run(debug=True)
