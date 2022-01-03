31b7795e2c92dc29cf494a62c717094167815a84
Damien ETHEVE <damien.etheve@telecomnancy.eu>
2022-01-03 16:53:29 +0100

    FEAT: add graph + dynamic similarite

--- similarites.py
+++ similarites.py
@@ -1,14 +1,17 @@
+from neo4j import data
 import pandas as pd
 import numpy as np
 from datetime import datetime
+from csv import writer
+import os
 
 def compute_matrix(output_matrix_path):
     start_time = datetime.now()
 
-    brut = pd.read_csv('datas/test.tab', sep='\t', keep_default_na=False)
-    domains = brut["Cross-reference (InterPro)"].str.split(';')
+    brut = pd.read_csv('datas/tostestas.tab', sep='\t', keep_default_na=False)
+    domains = brut["Crossreference"].str.split(';')
     dataset = pd.DataFrame({'Entry': brut["Entry"], 'Domains': domains[:]})
-
+    #dataset.index = dataset["Entry"]
     print(dataset)
 
     stageDSCreated_time = datetime.now()
@@ -29,36 +32,53 @@ def compute_matrix(output_matrix_path):
         else:
             return 0
 
-
-    def computeMatriceSimilarites(dataset):
+    def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+        with open(file_name, 'a+', newline='') as write_obj:
+            # Create a writer object from csv module
+            csv_writer = writer(write_obj)
+            # Add contents of list as last row in the csv file
+            csv_writer.writerow(list_of_elem)
+        
+    def computeMatriceSimilarites(dataset, protName):
         nomsProteines = list(dataset['Entry'])
-        matrice = np.zeros((len(nomsProteines),len(nomsProteines)+1), dtype=object) # np.zeros(nb lignes, nb colonnes)
-        for i in range(len(nomsProteines)):
-            matrice[i][0] = nomsProteines[i]
-            for j in range(i,len(nomsProteines)):
-                temp = compare(dataset['Domains'][i],dataset['Domains'][j])
-                matrice[i][j+1] = temp
-                matrice[j][i+1] = temp
-            print(i, '/', len(nomsProteines), ' proteins treated')
+        #print(nomsProteines.index(protName))
+        matrice = np.zeros((1,len(nomsProteines)+1), dtype=object) # np.zeros(nb lignes, nb colonnes)
+        #print(matrice)
+        #for i in range(len(nomsProteines)):
+        #index = dataset.loc[protName, :]
+        #print("INDEX", index)
+        index = nomsProteines.index(protName)
+        print("Index: ",index)
+        matrice[0][nomsProteines.index(protName)] = protName
+        print(matrice[0])
+        for j in range(0,len(nomsProteines)):
+            temp = compare(dataset['Domains'][index],dataset['Domains'][j])
+            matrice[0][j+1] = temp
+            #matrice[j][0+1] = temp
+        #print(0, '/', len(nomsProteines), ' proteins treated')
         return matrice
 
-    mat = computeMatriceSimilarites(dataset)
-    print(mat)
-    print(mat[:,1:].sum())
 
-    stageComputeMatrix_time = datetime.now()
-    print('Calcul de toutes les similarités : ', stageComputeMatrix_time - start_time, ' (hh:mm:ss.ms)')
+    protname = "P5"
+    mat = computeMatriceSimilarites(dataset, protname)[0]
+    mat[0] = protname
+    print(mat[0])
+    #print("qsgqsghqshqfshqsfh",list(mat[0]))
+    #print(list(dataset["Entry"]))
+    
+    
+    if os.path.isfile("datas/matrix_tri.csv"):
+        # fichier existe
+        # ajouter une line au fichier
+        file = pd.read_csv('datas/matrix_tri.csv', sep=',', keep_default_na=False)
+        # check if line exist
+        if not (file.index == protname).any():
+            append_list_as_row('datas/matrix_tri.csv', list(mat))
+    else:
+        # fichier existe pas        
+        append_list_as_row('datas/matrix_tri.csv', list(dataset["Entry"]))
+        append_list_as_row('datas/matrix_tri.csv', list(mat))
+        
 
-    newDF = pd.DataFrame(mat[:,1:], index=mat[:,0], columns=list(dataset['Entry']))
-    print(newDF)
-
-    stageDataframeCreation_time = datetime.now()
-    print('Création du dataframe : ', stageDataframeCreation_time - start_time, ' (hh:mm:ss.ms)')
-
-    newDF.to_csv(path_or_buf=output_matrix_path)
-
-    stageCSVExport_time = datetime.now()
-    print('Création du dataset : ', stageDSCreated_time - start_time, ' (hh:mm:ss.ms)')
-    print('Calcul de toutes les similarités : ', stageComputeMatrix_time - start_time, ' (hh:mm:ss.ms)')
-    print('Création du dataframe : ', stageDataframeCreation_time - start_time, ' (hh:mm:ss.ms)')
-    print('Temps d\'exécution total :', stageCSVExport_time - start_time)
+compute_matrix("datas/matrix_tri.csv")
