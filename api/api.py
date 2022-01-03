5f0f2a6258d9023891d46ac4331e5fea2bc55210
ben_h <79261542+gohu994@users.noreply.github.com>
2022-01-03 20:22:26 +0100

    solving CORS issue when calling api

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, abort, Response
+from flask_cors import CORS
 from flask_restful import Api, Resource
 # TODO:  add swagger specs
 # from flask_restful_swagger import swagger
@@ -7,6 +8,8 @@ import json
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app, origins="http://localhost:3000", allow_headers=[
+    "Content-Type"])
 
 # @api.route("/users/")
 class Protein(Resource):
@@ -15,7 +18,7 @@ class Protein(Resource):
       Insert protein similarity in neo4j
     """
     data = request.get_json()
-    print(data['name'])
+    print(data)
     
     # Use the function
     
