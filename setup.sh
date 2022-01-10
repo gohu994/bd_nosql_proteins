6829564c75ffbfe4fa56f52b59aafc58731c5675
ben_h <79261542+gohu994@users.noreply.github.com>
2022-01-10 16:25:50 +0100

    script fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-rm -rf ~/neo4j
+sudo rm -rf ~/neo4j
 
-docker run --name=my-prot --publish=7474:7474 --publish=7687:7687 --volume=$HOME/neo4j/data:/data  --volume=$HOME/neo4j/import:/import  --env NEO4J_dbms_memory_pagecache_size=2G  --env=NEO4J_AUTH=none   neo4j
+docker run -d --publish=7474:7474 --publish=7687:7687 --volume=$HOME/neo4j/data:/data  --volume=$HOME/neo4j/import:/import  --env NEO4J_dbms_memory_pagecache_size=2G  --env=NEO4J_AUTH=none   neo4j
 
-docker cp ./datas my-prot:/import
+sudo cp ./datas/fulldata.tab ~/neo4j/import
 
 python3 api.py
